0165d7160449f8fa94dea4957464fc78a5d63db1
KhalilKahwaji <khellowz@gmail.com>
2025-10-17 15:41:08 +0300

    [Testing] SIGN IN FIX TEST

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,33 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:interactive_map/Backend/Login.dart';
 import 'package:interactive_map/Homepages/firstPage.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:interactive_map/theme/colors.dart';
-import 'Backend/auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await signOut();
+  // REMOVE: await signOut();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       locale: const Locale('ar', 'MA'),
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
-        Locale('en', ''),
-        Locale('ar', 'MA'),
-      ],
-      debugShowCheckedModeBanner: false,
-      title: 'Interactive Map',
+      supportedLocales: const [Locale('ar', 'MA')],
       theme: ThemeData(
-        brightness: Brightness.light,
         scaffoldBackgroundColor: AppColors.background,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.primary,
-          brightness: Brightness.light,
-        ),
         appBarTheme: const AppBarTheme(
           elevation: 0,
           backgroundColor: AppColors.background,
